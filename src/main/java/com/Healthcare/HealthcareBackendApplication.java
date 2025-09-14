c484a418cac7dec881582e4e5b187ba6549e88fb
SACHINUPADHYAY414 <upadhyaysachin414@gmail.com>
2025-09-14 14:56:06 +0530

    project update

--- src/main/java/com/Healthcare/HealthcareBackendApplication.java
+++ src/main/java/com/Healthcare/HealthcareBackendApplication.java
@@ -2,7 +2,9 @@ package com.healthcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class HealthcareBackendApplication {
     public static void main(String[] args) {
